ea92ac6bf34c76b30ddd704bcefd14f8dc0eb052
yang <874733577@qq.com>
2020-02-13 02:02:08 +0800

    添加api测试用例

---
+++ src/test/java/ApiTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+import top.beliefyu.fstream.client.api.DataStream;
+import top.beliefyu.fstream.client.api.function.MapperFunction;
+
+/**
+ * ApiTest
+ *
+ * @author yuxinyang
+ * @version 1.0
+ * @date 2020-02-13 01:30
+ */
+class ApiTest {
+
+    @Test
+    void buildTest(){
+        DataStream<Integer> dataStream = new DataStream<>(() -> 110);
+        DataStream<Long> map = dataStream.map(Integer::longValue);
+        System.out.println(map);
+    }
+
+}
