44eac739fc02b86bbad8ced50133d39dd684dc1f
yang <874733577@qq.com>
2020-02-17 02:58:39 +0800

    rpc service

--- src/main/java/top/beliefyu/fstream/common/grpc/GrpcClient.java
+++ src/main/java/top/beliefyu/fstream/common/grpc/GrpcClient.java
@@ -1,5 +1,14 @@
 package top.beliefyu.fstream.common.grpc;
 
+import io.grpc.Channel;
+import io.grpc.netty.NegotiationType;
+import io.grpc.netty.NettyChannelBuilder;
+import org.apache.log4j.Logger;
+import top.beliefyu.fstream.rpc.*;
+
+import java.util.Iterator;
+
+
 /**
  * GrpcClient
  *
@@ -8,4 +17,37 @@ package top.beliefyu.fstream.common.grpc;
  * @date 2020-02-17 01:18
  */
 public class GrpcClient {
+    private static final Logger logger = Logger.getLogger(GrpcClient.class);
+
+    private final RpcServerGrpc.RpcServerBlockingStub blockingStub;
+    private final RpcServerGrpc.RpcServerStub asyncStub;
+
+
+    public GrpcClient(Channel channel) {
+        blockingStub = RpcServerGrpc.newBlockingStub(channel);
+        asyncStub = RpcServerGrpc.newStub(channel);
+    }
+
+    public GrpcClient(String host, int port) {
+        this(NettyChannelBuilder.forAddress(host, port)
+                .negotiationType(NegotiationType.PLAINTEXT)
+                .build());
+    }
+
+    public Iterator<MessageResponse> pullMessage() {
+        return blockingStub.pullMessage(MessageRequest.newBuilder().build());
+    }
+
+    public HeartBeatResponse doHeartBeatTest() {
+        return blockingStub.doHeartBeatTest(buildHeartBeatRequest());
+    }
+
+    private HeartBeatRequest buildHeartBeatRequest() {
+        return HeartBeatRequest.newBuilder()
+                .setTimestamp(System.currentTimeMillis())
+                .setMsg("ping")
+                .build();
+    }
+
+
 }
--- src/main/java/top/beliefyu/fstream/common/grpc/GrpcServer.java
+++ src/main/java/top/beliefyu/fstream/common/grpc/GrpcServer.java
@@ -1,5 +1,8 @@
 package top.beliefyu.fstream.common.grpc;
 
+import io.grpc.stub.StreamObserver;
+import top.beliefyu.fstream.rpc.*;
+
 /**
  * GrpcServer
  *
@@ -7,5 +10,29 @@ package top.beliefyu.fstream.common.grpc;
  * @version 1.0
  * @date 2020-02-17 01:17
  */
-public class GrpcServer {
+public class GrpcServer extends RpcServerGrpc.RpcServerImplBase {
+    @Override
+    public void pullMessage(MessageRequest request, StreamObserver<MessageResponse> responseObserver) {
+        responseObserver.onNext(streamToMessage());
+        responseObserver.onCompleted();
+    }
+
+    private MessageResponse streamToMessage() {
+        //todo 流数据获取与转换
+        return null;
+    }
+
+
+    @Override
+    public void doHeartBeatTest(HeartBeatRequest request, StreamObserver<HeartBeatResponse> responseObserver) {
+        responseObserver.onNext(buildHeartBeatResponse());
+        responseObserver.onCompleted();
+    }
+
+    private HeartBeatResponse buildHeartBeatResponse() {
+        return HeartBeatResponse.newBuilder()
+                .setTimestamp(System.currentTimeMillis())
+                .setMsg("pong")
+                .build();
+    }
 }
