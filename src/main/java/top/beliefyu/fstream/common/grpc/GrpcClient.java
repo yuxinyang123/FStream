091dcc543deff38bed8c2473024f6a86ad778088
yang <874733577@qq.com>
2020-02-17 01:57:13 +0800

    add rpc proto

---
+++ src/main/java/top/beliefyu/fstream/common/grpc/GrpcClient.java
@@ -0,0 +1,11 @@
+package top.beliefyu.fstream.common.grpc;
+
+/**
+ * GrpcClient
+ *
+ * @author yuxinyang
+ * @version 1.0
+ * @date 2020-02-17 01:18
+ */
+public class GrpcClient {
+}
---
+++ src/main/java/top/beliefyu/fstream/common/grpc/GrpcServer.java
@@ -0,0 +1,11 @@
+package top.beliefyu.fstream.common.grpc;
+
+/**
+ * GrpcServer
+ *
+ * @author yuxinyang
+ * @version 1.0
+ * @date 2020-02-17 01:17
+ */
+public class GrpcServer {
+}
