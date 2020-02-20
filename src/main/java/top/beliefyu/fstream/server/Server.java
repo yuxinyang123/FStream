b79695760ff1f831a417fe2b4c6988a0465d36fa
yang <874733577@qq.com>
2020-02-21 01:26:56 +0800

    server module

--- src/main/java/top/beliefyu/fstream/server/Server.java
+++ src/main/java/top/beliefyu/fstream/server/Server.java
@@ -1,5 +1,9 @@
 package top.beliefyu.fstream.server;
 
+import top.beliefyu.fstream.common.grpc.GrpcServer;
+import top.beliefyu.fstream.common.grpc.ServerGrpcService;
+import top.beliefyu.fstream.common.zk.ZkClient;
+
 /**
  * Server
  *
@@ -8,7 +12,27 @@ package top.beliefyu.fstream.server;
  * @date 2020-02-18 21:05
  */
 public class Server {
-    public static void main(String[] args) {
 
+    private static ZkClient zkClient;
+
+    private static GrpcServer grpcServer;
+
+    public static void main(String[] args) {
+        zkClient = new ZkClient("127.0.0.1", 2181);
+        grpcServer = new GrpcServer(6666, new ServerGrpcService());
+        start();
     }
+
+
+    private static void start() {
+        zkClient.start();
+        grpcServer.start();
+        try {
+            grpcServer.blockUntilShutdown();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
---
+++ src/main/java/top/beliefyu/fstream/server/ServerService.java
@@ -0,0 +1,27 @@
+package top.beliefyu.fstream.server;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * ServerService
+ *
+ * @author yuxinyang
+ * @version 1.0
+ * @date 2020-02-20 20:02
+ */
+public class ServerService {
+
+    private Map<String, Object> nodeHostMap = new HashMap<>();
+    private Map<String, Integer> hostWeightMap = new TreeMap<>();
+
+    public void getAllRegistedServices() {
+    }
+
+    public void generatePhysicsExecution() {
+    }
+
+    public void distributePhysicsExecution() {
+    }
+}
