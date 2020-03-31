11ced0843003abbacea3091e05642e2d0714125f
yang <874733577@qq.com>
2020-03-31 21:48:43 +0800

    gitignore

--- src/main/java/top/beliefyu/fstream/common/netty/NettyServer.java
+++ src/main/java/top/beliefyu/fstream/common/netty/NettyServer.java
@@ -9,12 +9,7 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.bytes.ByteArrayDecoder;
 import io.netty.handler.codec.bytes.ByteArrayEncoder;
-import io.netty.handler.timeout.IdleState;
-import io.netty.handler.timeout.IdleStateEvent;
-import io.netty.handler.timeout.IdleStateHandler;
 import io.netty.util.ReferenceCountUtil;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * NettyServer
