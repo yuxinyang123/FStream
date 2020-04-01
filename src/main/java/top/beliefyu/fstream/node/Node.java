cccd5cbe813e4aa9d3658613c27bd3981cebeb40
yang <874733577@qq.com>
2020-04-02 02:12:55 +0800

    Node Server

--- src/main/java/top/beliefyu/fstream/node/Node.java
+++ src/main/java/top/beliefyu/fstream/node/Node.java
@@ -1,5 +1,12 @@
 package top.beliefyu.fstream.node;
 
+import top.beliefyu.fstream.common.grpc.GrpcServer;
+import top.beliefyu.fstream.common.zk.ZkClient;
+import top.beliefyu.fstream.node.grpc.NodeGrpcService;
+import top.beliefyu.fstream.node.service.NodeService;
+import top.beliefyu.fstream.server.grpc.ServerGrpcService;
+import top.beliefyu.fstream.server.service.ServerService;
+
 /**
  * Node
  *
@@ -8,7 +15,39 @@ package top.beliefyu.fstream.node;
  * @date 2020-02-23 03:02
  */
 public class Node {
-    public static void main(String[] args) {
 
+    private static ZkClient zkClient;
+
+    private static GrpcServer grpcServer;
+
+    private static NodeService nodeService = new NodeService();
+
+    public static void main(String[] args) {
+        init();
+        registerInZk();
+        grpcServerStartAndBlockUntilShutdown();
+    }
+
+    private static void init() {
+        zkClient = new ZkClient("192.168.2.129", 2181);
+        grpcServer = new GrpcServer(6666, new NodeGrpcService());
+        zkClient.start();
+    }
+
+    private static void registerInZk() {
+
+    }
+
+    private static void grpcServerStartAndBlockUntilShutdown() {
+        try {
+            grpcServer.start();
+            grpcServer.blockUntilShutdown();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static NodeService getNodeService() {
+        return nodeService;
     }
 }
